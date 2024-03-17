f3254f9de9820648bd62c517afd531e3dfe50aa7
Kunle Ogunjimi <kdotkid3@gmail.com>
2024-03-17 12:54:17 +0000

    basic page tutorial

---
+++ app/planner/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-indigo-500">
+        <div>
+            <h1 className="text-3xl">Building A Business</h1>
+        </div>
+    </main>
+    );
+}
